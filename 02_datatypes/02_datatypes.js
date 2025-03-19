e336de4b23ee9e711e83124625cc4d1603f54510
Rahul Das <153532433+RahulDas77@users.noreply.github.com>
2025-03-20 00:32:31 +0530

    Delete 02_datatypes directory

--- 02_datatypes/02_datatypes.js
+++
@@ -1,9 +0,0 @@
-"use strict"; //treats all js code as newer version
-
-let name= "rahul" //string
-let age = 21  //number
-let connection = false  //boolean
-let work = null //empty
-let money;  //undefined
-
-console.log(typeof 25.3);

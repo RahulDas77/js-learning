524c10fd3c0eb1ba4924a00bcc4f2104b0da852c
Rahul Das <153532433+RahulDas77@users.noreply.github.com>
2025-03-20 00:31:51 +0530

    Add files via upload

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treats all js code as newer version
+
+let name= "rahul" //string
+let age = 21  //number
+let connection = false  //boolean
+let work = null //empty
+let money;  //undefined
+
+console.log(typeof 25.3);
---
+++ 01_basics/03_Conversion datatypes.js
@@ -0,0 +1,25 @@
+let score = 36
+console.log(typeof score) //number
+
+let num = "36"
+console.log(typeof num); //string
+
+let run = Number (num)
+console.log(typeof run); //number
+
+let money = null
+console.log(money); //null
+
+let currency = Number(money)
+console.log(currency); //0
+
+let length;
+console.log(length); //undefined
+
+let username = "raul123"
+let id = Number(username)
+console.log(id); //NaN
+
+//true <=> 1 and false <=> 0 visce versa
+//"" => false and "anything" => true
+

d9e9664df97618a3f9248a11f321455049a6d508
Rahul Das <153532433+RahulDas77@users.noreply.github.com>
2025-03-20 00:33:48 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 12335
+let accName= "rahuldas"
+var city = "kolkata"
+state = "west bengal"
+
+console.table([accountId,accName,city,state]);
+
+/*
+const keyword restrict dev from changing its value after its declared
+let key word should be used instead of var 
+always declare variable with let and const and declare it directly.
+*/
